5b1613c4b76389a7543d8b1f01bee47110681168
Jeannie Blakely <jeanbblakely@gmail.com>
2019-07-31 18:48:02 -0400

    problems with CORS policy

--- src/app/apiservice.service.ts
+++ src/app/apiservice.service.ts
@@ -10,6 +10,8 @@ export class ApiserviceService {
   constructor(private httpClient: HttpClient) { }
 
   getItem(apikey, barcode) {
-    return this.httpClient.get('https://api-na.hosted.exlibrisgroup.com/almaws/v1/items?item_barcode={barcode}&apikey={apikey}');
+    console.log('https://api-na.hosted.exlibrisgroup.com/almaws/v1/items?item_barcode=' + barcode + '&apikey=' + apikey);
+    this.httpClient.head('Access-Control-Allow-Origin');
+    return this.httpClient.get('https://api-na.hosted.exlibrisgroup.com/almaws/v1/items?item_barcode=' + barcode + '&apikey=' + apikey);
   }
 }
